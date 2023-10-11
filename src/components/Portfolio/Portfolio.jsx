bf9cc22b0577b4c86d720b17440c85215251ea5c
Irina <i.sus2011@yandex.ru>
2023-10-11 15:34:25 +0300

    fix: Обновить рабочие ссылки

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -21,7 +21,7 @@ const Portfolio = () => {
 
           </li>
           <li className='portfolio__item hover'>
-            <a className='portfolio__link' rel='noreferrer' href='https://github.com/i-suslova/react-mesto-api-full-gha/' target='_blank'>
+            <a className='portfolio__link' rel='noreferrer' href='https://i-suslova.github.io/mesto-react/' target='_blank'>
               Одностраничное приложение <span className='portfolio__span'>↗</span>
             </a>
           </li>
